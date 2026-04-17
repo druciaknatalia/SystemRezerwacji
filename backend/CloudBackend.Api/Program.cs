e1892c63eb4e9b1c18e937f20ccb61b208df824a
druciaknatalia <natalia.druciak01@gmail.com>
2026-04-17 03:10:51 +0200

    Artefakt 7: Key Vault, Managed Identity, usuniecie hasel z kodu

--- backend/CloudBackend.Api/Program.cs
+++ backend/CloudBackend.Api/Program.cs
@@ -1,10 +1,20 @@
 using CloudBackend.Api.Data;
 using Microsoft.EntityFrameworkCore;
+using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Key Vault
+var keyVaultUrl = builder.Configuration["KeyVaultUrl"];
+if (!string.IsNullOrEmpty(keyVaultUrl))
+{
+    builder.Configuration.AddAzureKeyVault(
+        new Uri(keyVaultUrl),
+        new DefaultAzureCredential());
+}
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-   options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -12,13 +22,13 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddCors(options =>
 {
-   options.AddPolicy("AllowFrontend", policy =>
-   {
-       policy
-           .AllowAnyOrigin()
-           .AllowAnyHeader()
-           .AllowAnyMethod();
-   });
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
 });
 
 var app = builder.Build();
